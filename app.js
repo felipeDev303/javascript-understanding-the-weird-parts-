39c8415c5aea424c1b87899f400172176027b7eb
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-12-02 03:15:06 -0300

    Agregar documentación sobre Faking Namespaces y mejorar ejemplos de objetos en app.js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-var a = 3 + 4 * 5;
-console.log(a);
+let persona = {
+  name: "Felipe",
+  age: 37,
+  saludar: function () {
+    console.log("Mi nombre es " + this.name);
+  },
+};
+
+console.log(persona.saludar());
+
+let animal = {
+  name: "León",
+  saludar() {
+    console.log("Soy un animal:" + this.name);
+  },
+};
+
+console.log(animal.saludar());
